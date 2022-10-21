8abf30ae87e14d5873c4399ad65a6d5ebd7f7817
rolandzwaga <scab@dds.nl>
2022-10-21 09:29:44 +0200

    remove generated files

--- examples/requestanimationframe/src/boot.ts
+++
@@ -1,8 +0,0 @@
-import { IEngineConfiguration, EngineFactory } from 'eligius';
-import * as engineConfig from './config-data.json';
-import WebpackResourceImporter from './webpack-resource-importer';
-const factory = new EngineFactory(new WebpackResourceImporter(), window);
-const writableConfig = JSON.parse(JSON.stringify(engineConfig.default)) as IEngineConfiguration;
-document.getElementById('config-text').value = JSON.stringify(writableConfig, null, 2);
-const engine = factory.createEngine(writableConfig);
-engine.init().then(()=> {console.log('Eligius engine ready for business');});
